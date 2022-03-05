2b34454157ddaeffaacd2c63f58115a792982425
Willo0ow <dhinc39@gmail.com>
2022-03-05 13:00:57 +0100

    basic gallery

--- src/components/MyWorkSection.vue
+++ src/components/MyWorkSection.vue
@@ -1,7 +1,25 @@
 <template>
   <section-frame id="work">
     <template #content>
-      <v-card-text>My work</v-card-text>
+      <v-row>
+        <v-col v-for="n in 8" :key="n" class="d-flex child-flex" cols="3">
+          <v-img
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="1"
+            class="grey lighten-2"
+          >
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-col>
+      </v-row>
     </template>
   </section-frame>
 </template>
