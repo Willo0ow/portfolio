2359f2a4939e5bf89b6d898985701ffe2f77d272
Willo0ow <dhinc39@gmail.com>
2022-03-05 12:02:00 +0100

    basic skills list

--- src/components/MySkillsSection.vue
+++ src/components/MySkillsSection.vue
@@ -1,7 +1,30 @@
 <template>
   <section-frame id="skills">
     <template #content>
-      <v-card-text>My skills</v-card-text>
+      <v-row>
+        <v-col>
+          <v-card-title>My skills</v-card-title>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-list>
+            <v-list-item v-for="(skill, idx) of skills" :key="idx">
+              <v-list-item-avatar>
+                <v-img :src="require(`@/assets/${skill.logo}`)"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content class="pl-5">
+                <v-list-item-title class="skill-name">{{
+                  skill.label
+                }}</v-list-item-title>
+                <v-list-item-subtitle class="skill-text">{{
+                  skill.text
+                }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
     </template>
   </section-frame>
 </template>
@@ -11,6 +34,57 @@ import SectionFrame from "./SectionFrame.vue";
 export default {
   name: "MySkillsSection",
   components: { SectionFrame },
+  data() {
+    return {
+      skills: [
+        {
+          label: "HTML",
+          logo: "html.png",
+          text: "Some nice description of the skill",
+        },
+        {
+          label: "CSS",
+          logo: "css.png",
+          text: "Some nice description of the skill",
+        },
+        {
+          label: "JavaScript",
+          logo: "js.png",
+          text: "Some nice description of the skill",
+        },
+        {
+          label: "PHP",
+          logo: "php.jpg",
+          text: "Some nice description of the skill",
+        },
+        {
+          label: "MySql",
+          logo: "mysql.jpeg",
+          text: "Some nice description of the skill",
+        },
+        {
+          label: "VueJs",
+          logo: "vue.png",
+          text: "Some nice description of the skill",
+        },
+        {
+          label: "Vuetify",
+          logo: "vuetify.svg",
+          text: "Some nice description of the skill",
+        },
+        {
+          label: "Laravel",
+          logo: "laravel.jpg",
+          text: "Some nice description of the skill",
+        },
+      ],
+    };
+  },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.skill-name,
+.skill-text {
+  text-align: start;
+}
+</style>
